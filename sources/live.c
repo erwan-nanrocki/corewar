4268ce2e23b7d123354595a1392e9b8a60e568d4
Erwan NANROCKI <enanrock@e1r2p16.42.fr>
2018-04-15 16:14:30 +0200

    version 1.2 (add some message in live, fork_e and lfork_e)

--- sources/live.c
+++ sources/live.c
@@ -6,7 +6,7 @@
 /*   By: enanrock <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 01:23:44 by enanrock          #+#    #+#             */
-/*   Updated: 2018/04/14 06:32:20 by enanrock         ###   ########.fr       */
+/*   Updated: 2018/04/15 16:02:53 by enanrock         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,14 @@ void	live(void *p1, void *p2, unsigned int arg[MAX_ARGS_NUMBER][5])
 	while (i < MAX_PLAYERS)
 	{
 		if (arg[0][1] == convert_pc_to_uint(mem->champ[i].id))
+		{
 			mem->champ[i].last_live = mem->cycles + 1;
+			ft_putstr("un processus dit que le joueur ");
+			ft_putunbr(i + 1);
+			ft_putchar('(');
+			ft_putstr(mem->champ[i].name);
+			ft_putendl(") est en vie");
+		}
 		i++;
 	}
 	read_head->last_live = mem->cycles + 1;
